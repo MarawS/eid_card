879890bcd9515500e0dfb93f6c9e0d5f0afc26f9
Marwah sulaiman <411007905@stu.ut.edu.sa>
2024-04-02 19:27:20 +0300

    eid card wetaan

--- index.php
+++ index.php
@@ -14,75 +14,81 @@ $images = $config['cards'];
     <link rel="icon" type="image/x-icon" href="<?php echo $config['favicon']; ?>">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.rtl.min.css">
     <link rel="stylesheet" href="bootstrap/css/style.css">
+    
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" />
     <title>تهنئة عيد الفطر</title>
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row align-items-center">
-            <div class="col-4 col-sm-4 text-start">
-                <img src="bootstrap/images/logos/pp_logo.svg" alt="" width="200">
-            </div>
-            <div class="col-4 col-sm-4">
+    
+    <div class="eid-mubarak col col-xs-2 col-md-4 .col-lg-2 bg-light d-flex flex-row justify-content-center" style="background-color: #f9f8f5; width: 100%;">
+      <div class="div">
+        <div class="overlap">
+          <div class="group ">
+          <img class=" col col-xs-2 col-md-2 col-lg-2 position-absolute end-0 md-6 w-10" src="bootstrap\images\logos\hilinky_logo.svg"  />
 
+          </div>
+          <div class="col col-xs-2 col-md-2 col-lg-2">
+          <p class="text-wrapper  ">
+            كل عام وأنتم بخير، أعاده الله علينا وعليكم بالصحة والسعادة والسلام.<br />
+            عيد فطر مبارك!
+          </p></div>
+          <img class="frame " src="bootstrap/images/bg/gray_frame.svg" />
+          <img class="img" src="bootstrap/images/bg/blue_frame.svg" />
+<div class="btn-group ">
+    <button type="button" class="btn dropdown-toggle text-white fw-bold col col-xs-2 col-md-2 col-lg-2" style="font-family: Cairo; font-size: 18px; font-style: normal;  " data-bs-toggle="dropdown" aria-expanded="false">
+      أختر تصميم
+    </button>
+    <ul class="dropdown-menu ">
+      <li><a class="dropdown-item" href="#">Action 1</a></li>
+      <li><a class="dropdown-item" href="#">Action 2</a></li>
+      <li><a class="dropdown-item" href="#">Action 3</a></li>
+    </ul>
+</div>
+          <div class="frame-wrapper ">
+            <div class="frame-3 ">
+              <p class="p ">
+                انضم إلينا في فعاليتنا الخاصة حيث يمكنك كتابة اسمك على التصميم، وبعد ذلك ستتمكن من مشاركته مع الجميع!
+              </p>
+              <div class="frame-3">
+                      
+                          <img class="profile-photo" src="bootstrap/images/logos/wetaan.jpeg" />
+                      
+                      
+                    
+                     
+                    
+              </div>
+              <div class="frame-5">
+                <div class="div-wrapper"><div class="text-wrapper-4">اكتب اسمك هنا</div></div>
+                <div class="frame-6"><div class="text-wrapper-5">احفظ الصورة</div></div>
+              </div>
             </div>
-            <div class="col-4 col-sm-4 text-end">
-                <img src="bootstrap/images/logos/2030_logo.png" alt="" width="150">
-            </div>
+          </div>
         </div>
-        <hr>
-        <br>
-        <h1 class="text-center title">اهلاً وسهلاً بكم</h1>
-        <p class="text-center description">اختر البطاقة التي تناسبك</p>
-        <br>
-        <form action="gen_image.php" id="cards_form" method="post" enctype="multipart/form-data">
-            <div class="row justify-content-center">
-                <?php foreach ($images as $image) { ?>
-                    <div class="col-auto">
-
-                        <div class="card-div">
-                            <input class="check-colour" type="radio" name="image_selected" id="" value="<?php echo $image; ?>" required>
-                            <img src="<?php echo $config['cards_dir'] . $image; ?>" alt="بطاقة تهنئة" class="border rounded" width="210">
-                        </div>
-                        <br>
-                        <br>
-                    </div>
-                <?php } ?>
+        <img class="line" src="img/line-44.svg" />
+        <div class="overlap-group-wrapper">
+          <div class="overlap-group-2">
+            <div class="group-2">
+              <div class="frame-7">
+                <img class="group-3" src="bootstrap\images\logos\hilinky_logo.svg" />
+                
+                <div class="text-wrapper-6">Hi.linky@wetaan.com</div>
+              </div>
             </div>
-            <br>
-            <div class="row text-center">
-                <div class="col-2">
+            <img class="text-center" src="bootstrap/images/logos/linkedin.svg" />
+            <img class="rounded" src="bootstrap/images/logos/x.svg" />
+            <img class="rounded" src="bootstrap/images/logos/instagram.svg" />
 
-                </div>
-                <div class="col-8">
-                    <input type="text" name="emp_name" id="" placeholder="أكتب أسمك هنا..." class="form-control" required>
-                    <br>
-                    <!-- <button type="submit" class="btn btn-success btn-lg" id="generate_card">إنشاء البطاقة</button> -->
-                    <button type="button" class="btn btn-success btn-lg button_card" id="review">معاينة</button>
-                    <button type="submit" class="btn btn-success btn-lg button_card" id="download">تحميل</button>
-                </div>
-                <div class="col-2">
-
-                </div>
-            </div>
-            <br>
-            <div class="row text-center">
-                <div class="col-4 col-sm-4">
-
-                </div>
-                <div class="col-4 col-sm-4">
-                    <div id="imageContainer">
-                        <!-- Image will be loaded dynamically here -->
-                    </div>
-                </div>
-                <div class="col-4 col-sm-4">
-
-                </div>
-            </div>
-        </form>
-        <br>
+          </div>
+        </div>
+        <img class="designed-by-karim-el" src="img/designed-by-karim-el-refai-nema-alnajjar.svg" />
+        <img class="developed-by-marwa" src="img/developed-by-marwa-sulaiman-fai-almutairi.svg" />
+      </div>
     </div>
-</body>
+  </body>
 <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 <script>
     document.getElementById('review').addEventListener('click', function(event) {
